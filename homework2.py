a0fbdb2e5b7efebad41a12055ae8c89ff3f0ac70
SlavonNovikov <vnovikov8666@gmail.com>
2024-07-15 22:24:54 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+
+Number_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_name="Python"
+Time_per_task=Number_of_hours_spent/Number_of_homework_completed
+print ("Курс:", Course_name, ", всего задач:",Number_of_homework_completed,", затрачено часов:",Number_of_hours_spent,", среднее время выполнения",Time_per_task,"часа.")
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+
