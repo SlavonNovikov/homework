73825cc5edec098257a6712dee88255e2e3bec4f
Vyacheslav <vnovikov8666@gmail.com>
2024-08-06 22:18:11 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def fake_divide(a, b):
+
+    if b==0:
+        return inf
+    else:
+        result = a / b
+        return a/b
---
+++ true_math.py
@@ -0,0 +1,6 @@
+def true_divide(a, b):
+    if b == 0:
+       result = "Ошибка"
+    else:
+        result = a / b
+    return result
