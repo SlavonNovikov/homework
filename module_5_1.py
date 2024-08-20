bfcc0da6110611582f50e584c7824eaa28786baf
Vyacheslav <vnovikov8666@gmail.com>
2024-08-20 09:56:58 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -16,13 +16,16 @@
 #
 
 
+
+
 class House:
     def __init__(self, name, number_of_floor):
+
         self.name = name
         self.number_of_floor = number_of_floor
 
     def go_to(self, new_floor):
-        if new_floor > self.number_of_floor:
+        if new_floor > self.number_of_floor or new_floor < 1:
             print("Такого этажа не существует")
         else:
             for i in range(1, new_floor + 1):
