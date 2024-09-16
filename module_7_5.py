143b46619ecf0a26adab1ee96a7dd919bca43723
Vyacheslav <vnovikov8666@gmail.com>
2024-09-16 19:58:05 +0300

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,30 @@
+#Файлы в операционной системе".
+#
+# Научиться применять методы os.walk, os.path.join, os.path.getmtime, os.path.dirname, os.path.getsize и использование модуля time для корректного отображения времени.
+#
+# Создайте новый проект или продолжите работу в текущем проекте.
+# Используйте os.walk для обхода каталога, путь к которому указывает переменная directory
+# Примените os.path.join для формирования полного пути к файлам.
+# Используйте os.path.getmtime и модуль time для получения и отображения времени последнего изменения файла.
+# Используйте os.path.getsize для получения размера файла.
+# Используйте os.path.dirname для получения родительской директории файла.
+
+
+import os
+import time
+
+print('Текущая директория:', os.getcwd())
+os.chdir(r'/Users/a1234/PycharmProjects/pythonProject6/TestDirectory1')
+directory = os.getcwd()
+print('Текущая директория:', directory)
+
+
+for root, dirs, files in os.walk(directory):
+  for file in files:
+    filepath = os.path.join(directory)
+    filetime = os.path.getmtime(file)
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = os.path.getsize(file)
+    parent_dir = os.path.dirname(filepath)
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},'
+          f' Родительская директория: {parent_dir}')
