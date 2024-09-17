8cfb148ddb9c933c886454e6c34b25f92c3d39f2
Vyacheslav <vnovikov8666@gmail.com>
2024-09-17 13:02:31 +0300

    Add files via upload

---
+++ praktika7.py
@@ -0,0 +1,22 @@
+import tkinter
+import os
+from tkinter import filedialog
+
+
+def file_select():
+    filename = filedialog.askopenfilename(initialdir='/', title='Выберите файл', filetypes=(('Текстовый файл', '.txt'),
+                                                                                            ('Все файлы', '*')))
+    text['text'] = text['text'] + ' ' + filename
+    os.startfile(filename)
+
+
+window = tkinter.Tk()
+window.title('Проводник')
+window.geometry('500x500')
+window.configure(bg='black')
+window.resizable(False, False)
+text = tkinter.Label(window, text="Файл:", width=50, height=5, background='silver', foreground='blue')
+text.grid(column=1, row=1)
+button_select = tkinter.Button(window, width=20, height=3, text='Выберите файл',foreground='blue',command=file_select)
+button_select.grid(column=1, row=2, pady=5)
+window.mainloop()
